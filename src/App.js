7d4b1c9793a648131c51affe509dc24ebc53d2be
BVADITYA <bv.aditya2003@gmail.com>
2023-10-12 12:45:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './calculator.css';
+
+function Calculator() {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="screen">{input}</div>
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('/')}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={handleClear}>C</button>
+          <button onClick={handleCalculate}>=</button>
+          <button onClick={() => handleClick('+')}>+</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="calculator-container">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
